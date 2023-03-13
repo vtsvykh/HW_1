475ce3987acf01f25d871be96554b24c4d1020f2
vtsvykh <v.tsvykh0@gmail.com>
2023-03-13 09:36:40 +0700

    *

--- main.py
+++ main.py
@@ -6,18 +6,18 @@ print(ru.NUMBER)
 n = int(input())
 
 date = datetime(2009, 9, 25)
-data_future = datetime(2009, 9, 25) + timedelta(n)   # Дата спустя n дней.
+data_future = datetime(2009, 9, 25) + timedelta(n)
 
-SPEED = 38_241                                       # Скорость миль в час.
-speed_1 = SPEED * 24                                 # Скорость миль в день.
-DISTANCE = 16_637_000_000                            # Расстояние зонда от солнца 25 сентября 2009 года.
-distance_miles = DISTANCE + (n*speed_1)              # Расстоние от солнца спустя n дней в милях.
-distance_km = distance_miles * 1.6934                # Расстоние от солнца спустя n дней в километрах.
-distance_au = distance_miles/92_955_781.16             # Расстоние от солнца астрономических единицах.
+SPEED = 38_241                                       # Speed of miles per hour.
+speed_1 = SPEED * 24                                 # Speed of miles per hour.
+DISTANCE = 16_637_000_000
+distance_miles = DISTANCE + (n*speed_1)              # Distance in miles.
+distance_km = distance_miles * 1.6934                # Distance in kilometres.
+distance_au = distance_miles/92_955_781.16           # Distance in au.
 
-DELAY = 299_792_458                                  # Задержка в м/с.
-delay_1 = DELAY * 3.6                                # Задержка км/ч.
-delay_wireless = distance_km/delay_1                 # Задержка беспроводной связи спустя n дней.
+DELAY = 299_792_458
+delay_1 = DELAY * 3.6                                # Delay in km per hour.
+delay_wireless = distance_km/delay_1
 
 print(n, ru.DISTANCE, distance_miles, ru.MILES)
 print(n, ru.DISTANCE, distance_km, ru.KM)
